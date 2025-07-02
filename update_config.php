46235fbee438fa50e5354b3f024df7d8e2eef7bb
Guilherme Dill <guilhermealceu@hotmail.com>
2025-07-02 19:39:28 -0300

    Delete update_config.php

--- update_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$data = json_decode(file_get_contents('php://input'), true);
-file_put_contents('jogos_log.json', json_encode($data));
-echo json_encode(['success' => true]);
-?>
